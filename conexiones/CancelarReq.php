f44dc60c6372d909516094602c7068797296cd39
unknown <cadicossamm@gmail.com>
2020-11-26 14:39:53 -0500

    aceptarReqSoporte

--- conexiones/CancelarReq.php
+++ conexiones/CancelarReq.php
@@ -4,6 +4,7 @@ require_once "./funciones.php";
 if(isset($_POST["req"])){
     $req = $_POST["req"];
     cancelar_req($req);
+    //var_dum($req)
     header("location: ../vistas/view_soporte.php");
 }else{
     echo "error en el post";
